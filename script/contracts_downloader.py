5c1511056b39d985b0d17ff57f076e502f64cb85
Siew Kam Onn <kosiew@gmail.com>
2024-04-24 18:47:48 +0800

    download contract files work

--- script/contracts_downloader.py
+++ script/contracts_downloader.py
@@ -47,11 +47,17 @@ class ContractsDownloadManager:
 
     def handle_file_download(self, address_path, contract_path, pbar, meta):
         try:
-            sourcecode = self.download_contract(address=address_path)
-            if not sourcecode[0]['SourceCode']:
+            data = self.download_contract(address=address_path)
+            if not data[0]['SourceCode']:
                 meta["empty"] += 1
             with open(contract_path, 'w') as fd:
-                json.dump(sourcecode[0], fd)
+                sourcecode = data[0]['SourceCode']
+                # sourcecode looke like '{{\r\n  "language": "Solidity",\r\n  "sources":........}}'
+                # We need to remove the first '{{' and the last '}}' to make it a valid JSON
+                sourcecode = sourcecode.replace('{{', '{', 1)  # Replace the first occurrence of '{{' with '{'
+                sourcecode = sourcecode[::-1].replace('}}'[::-1], '}'[::-1], 1)[::-1]  # Replace the first occurrence of '}}' from the end with '}'
+ 
+                json.dump(sourcecode, fd)
         except Exception as e:
             self.not_valid.append(address_path)
             with open('not_valid.json', 'w') as fd:
@@ -83,32 +89,39 @@ class ContractsDownloadManager:
                 contract_path = Path(self.output_dir, f"{address_path}.json")
                 print(f"Downloading contract {i}/{address_count}: {address_path}")
                 self.handle_file_download(address_path, contract_path, pbar, meta)
-                pbar.update(0.5)
                 self.extract_sol_files(contract_path)
+                pbar.update(1)
 
         if self.not_valid:
             with open('not_valid.json', 'w') as fd:
                 json.dump(self.not_valid, fd)    
 
     def extract_sol_files(self, json_file_path):
-        # Determine the full path to the output directory, relative to the current working directory
-        output_dir = self.output_dir
+        # Create the output directory if it does not exist
+        output_path = os.path.join(self.output_dir, 'contracts')
+        os.makedirs(output_path, exist_ok=True)
         
         # Open and read the JSON file
-        with open(json_file_path, 'r') as file:
-            data = json.load(file)
-            # Parse the "SourceCode" object
-            source_code = json.loads(data['SourceCode'])
+        try:
+            with open(json_file_path, 'r') as file:
+                data = json.load(file)
             
+            # Parse the "SourceCode" object assuming it's a string that needs to be JSON parsed
+            source_code = json.loads(data)
+
             # Iterate over the items in the "sources" key
             for path, file_info in source_code['sources'].items():
                 # Prepare the file's full path
-                full_path = os.path.join(output_dir, path)
-                # Make sure the directory exists
+                full_path = os.path.join(output_path, path)
+                # Ensure the directory exists
                 os.makedirs(os.path.dirname(full_path), exist_ok=True)
                 # Write the file content to the corresponding .sol file
                 with open(full_path, 'w', encoding='utf-8') as sol_file:
                     sol_file.write(file_info['content'])
+        except json.JSONDecodeError as e:
+            print("Failed to decode JSON:")
+        except KeyError as e:
+            print("Key error - check JSON structure:", e)
                     
     @backoff.on_exception(backoff.expo,
                           (EmptyResponse, BadRequest, ConnectionRefused),
